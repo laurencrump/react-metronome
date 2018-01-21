3a087cdf500b3d75471ee2a3051f6edee0f94dd6
Lauren Bocook <laurenvcrump@gmail.com>
2018-01-20 19:41:49 -0500

    adding state

--- src/Metronome.js
+++ src/Metronome.js
@@ -2,9 +2,24 @@ import React, { Component } from 'react';
 import './Metronome.css';
 
 class Metronome extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			playing: false,
+			count: 0,
+			bpm: 100,
+			beatsPerMeasure: 4
+		};
+	}
+
+	handleBpmChange = event => {
+		const bpm = event.target.value;
+		this.setState({ bpm });
+	}
+
 	render() {
-		let bpm = 100;
-		let playing = false;
+		const { playing, bpm } = this.state;
 
 		return(
 			<div className='metronome'>
@@ -15,6 +30,7 @@ class Metronome extends Component {
 						min="60"
 						max="240"
 						value={bpm}
+						onChange={this.handleBpmChange}
 					/>
 					<button>
 						{playing ? 'Stop' : 'Start'}
